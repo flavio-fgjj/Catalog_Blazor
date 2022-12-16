64dfceb568959811251184212e4c370f3034f23b
Flavio A Alvarenga <flavio.fgjj@live.com>
2022-12-16 17:47:45 -0300

    component razor added for add new category

--- Catalog_Blazor/Shared/Models/Category.cs
+++ Catalog_Blazor/Shared/Models/Category.cs
@@ -5,9 +5,11 @@ namespace Catalog_Blazor.Shared.Models;
 public class Category
 {
     public int CategoryId { get; set; }
+    [Required(ErrorMessage = "Category name is required")]
     [MaxLength(100)]
     public string? Name { get; set; }
     [MaxLength(200)]
+    [Required(ErrorMessage = "Description is required")]
     public string? Description { get; set; }
 
     public ICollection<Product>? Products { get; set; }
