2c72e1f3b9bf2728d264e535dd4ce09d511fedaf
Flavio A Alvarenga <flavio.fgjj@live.com>
2022-12-16 15:25:40 -0300

    Models Category and Product created and also the relationship between them

---
+++ Catalog_Blazor/Shared/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog_Blazor.Shared.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public string? ImageUrl { get; set; }
+
+        // indicate the relationship 
+        public int CategoryId { get; set; }
+        // navigation prop
+        public virtual Category? Category { get; set; }
+    }
+}
