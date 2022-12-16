0c61e1e43f4ae4195407e9da83ee87ad0aa63354
Flavio A Alvarenga <flavio.fgjj@live.com>
2022-12-16 16:55:19 -0300

    Category Controller added + refactor at dbContext

--- Catalog_Blazor/Server/Context/AppDbContext.cs
+++ Catalog_Blazor/Server/Context/AppDbContext.cs
@@ -11,6 +11,6 @@ public class AppDbContext : DbContext
 
     }
 
-    public DbSet<Category>? Categories { get; set; }
-    public DbSet<Product>? Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
 }
---
+++ Catalog_Blazor/Server/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Catalog_Blazor.Server.Context;
+using Catalog_Blazor.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog_Blazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        // ctor
+        public CategoryController(AppDbContext context) // dependency injection
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> Get() => await context.Categories.AsNoTracking().ToListAsync();
+
+        [HttpGet("{id}", Name = "GetCategory")]
+        public async Task<ActionResult<Category>> Get(int id)
+        {
+            var result = await context.Categories.FirstOrDefaultAsync(x => x.CategoryId == id);
+            return result != null ? result : NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> Post(Category category)
+        {
+            context.Add(category);
+ 
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("GetCategory", new { id = category.CategoryId }, category);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Category>> Put(Category category)
+        {
+            context.Entry(category).State = EntityState.Modified;
+
+            await context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Category>> Delete(int id)
+        {
+            var category = new Category { CategoryId = id };
+            context.Remove(category);
+
+            await context.SaveChangesAsync();
+            return Ok(category);
+
+        }
+    }
+}
